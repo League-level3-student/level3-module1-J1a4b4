e6b9daf60811b835a4d632769ac8b7a2e07fe4be
J1a4b4 <46267154+J1a4b4@users.noreply.github.com>
2020-04-25 13:11:10 -0700

    MurderOfCrows, RemovingStuffFromArrayLists, LongChipCompetition

---
+++ src/_05_LongChipCompetition.java
@@ -0,0 +1,88 @@
+package _01_IntroToArrayLists;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class _05_LongChipCompetition {
+	/**
+	 * The Beatles are eating lunch and playing a game to see who has the longest
+	 * chip. (In England, french fries are called "chips".) * Find the Beatle with
+	 * the longest chip. You may not edit the Chip or Beatle classes. Make sure to
+	 * initialize The Beatles before you start your search. *
+	 **/
+	private ArrayList<Beatle> theBeatles = new ArrayList<Beatle>();
+
+	public static void main(String[] args) {
+		_05_LongChipCompetition lcc = new _05_LongChipCompetition();
+		lcc.initializeBeatles();
+		double length = 0;
+		int positionI = 0;
+		int positionII = 0;
+		int positionKey = 0;
+		for (int i = 0; i < lcc.theBeatles.size(); i++) {
+			for (int j = 0; j < lcc.theBeatles.get(i).getChips().size(); j++) {
+				if (lcc.theBeatles.get(i).getChips().get(j).getLength() > length) {
+					length = lcc.theBeatles.get(i).getChips().get(j).getLength();
+					positionI = i + 1;
+					positionII = j + 1;
+					positionKey = i;
+				}
+			}
+		}
+		System.out.println("The Beatle with the longest chip is " + lcc.theBeatles.get(positionKey).getName() + " (" + positionI + "." + positionII + ").");
+	}
+	
+	private void initializeBeatles() {
+		Beatle george = new Beatle("George");
+		Beatle john = new Beatle("John");
+		Beatle paul = new Beatle("Paul");
+		Beatle ringo = new Beatle("Ringo");
+		theBeatles.add(george);
+		theBeatles.add(john);
+		theBeatles.add(paul);
+		theBeatles.add(ringo);
+	}
+	
+	public ArrayList<Beatle> getTheBand(){
+		return theBeatles;
+	}
+}
+
+class Beatle {
+	private String name;
+	private ArrayList<Chip> chips = new ArrayList<Chip>();
+
+	public Beatle(String name) {
+		this.name = name;
+		initializePlateOfChips();
+	}
+
+	private void initializePlateOfChips() {
+		int numberOfChips = new Random().nextInt(100);
+		for (int i = 0; i < numberOfChips; i++) {
+			chips.add(new Chip(new Random().nextDouble() * 10));
+		}
+	}
+
+	public ArrayList<Chip> getChips() {
+		return this.chips;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
+
+class Chip {
+	private double length;
+
+	public double getLength() {
+		return length;
+	}
+
+	Chip(double d) {
+		this.length = d;
+	}
+}
